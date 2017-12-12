90ac0262a37d4e6b335b822b1b63ea74a73d36a1
Louvian Lyndal <louvianlyndal@gmail.com>
2017-12-12 21:41:48 +0700

    Update validator

--- src/Pururin/Validator.php
+++ src/Pururin/Validator.php
@@ -22,7 +22,51 @@ class Validator
 	public function run()
 	{
 		if ($this->status === "pending") {
-			
+			print "Redownload... $this->url\n";
+			try {
+				$app = new PururinCrawler(
+					[
+						"save_directory" => PURURIN_DATA,
+						"manga_url"		 => $this->url,
+						"offset"		 => $this->offset
+					]
+				);
+				if ($app->run()) {
+					$st = DB::prepare(
+						"INSERT INTO `pururin_main_data` (`id`, `title`, `info`, `origin_link`, `created_at`, `updated_at`) VALUES (:id, :title, :info, :origin_link, :created_at, :updated_at);"
+					);
+					$st->execute(
+						array_merge(
+							$data = $app->getResult(), 
+							[
+								"created_at" => date("Y-m-d H:i:s"), 
+								"updated_at" => null
+							]
+						)
+					);
+					$data['info'] = json_decode($data['info'], true);
+					if (isset($data['info']['Contents'])) {
+						$query = "INSERT INTO `pururin_genres` (`id`,`genre`) VALUES ";
+						$i = 0;
+						$queryValue = [
+							":id" => $data['id']
+						];
+						foreach ($data['info']['Contents'] as $val) {
+							$query .= "(:id, :genre{$i}),";
+							$queryValue[':genre'.($i++)] = $val;
+						}
+						$st = DB::prepare(rtrim($query, ","));
+						$exe = $st->execute($queryValue);
+					}
+				}
+				print "Success... $this->url\n";
+				return true;
+			} catch (\Exception $e) {
+				print "Failed... $this->url\n";
+				return false;
+			}
+		} else {
+			return "Valid";
 		}
 	}
 }
